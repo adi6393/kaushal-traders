b3b6afeffc1a8b26ebfadb746b100d367222d200
adi6393 <adikaushal555@gmail.com>
2024-10-25 18:45:36 +0530

    add JS file

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript function to display a special message
+function displayMessage() {
+    const specialMessage = document.getElementById('specialMessage');
+    specialMessage.innerText = "Thank you for shopping with us! Enjoy your grocery experience.";
+    specialMessage.style.display = "block";
+}
